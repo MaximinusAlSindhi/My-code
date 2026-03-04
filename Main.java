2d9dea821586260d54eee8c596cda373e3418d0a
MaximiniusAlSindhi <m.ayangaara@gmail.com>
2026-03-04 20:46:05 +0500

    wrote a new node finder which used no recursion or stacks or queues

---
+++ Main.java
@@ -0,0 +1,234 @@
+import java.util.Queue;
+import java.util.Stack;
+import java.util.LinkedList;
+
+
+
+public class Main {
+ public static void main(String args[]){
+ Node a = new Node(10, "A");
+          Node b = new Node(8, "B");
+          Node c = new Node(14, "C");
+          Node d = new Node(6, "D");
+          Node e = new Node(9, "E");
+          Node f = new Node(12, "F");
+          Node g = new Node(16, "G");
+          Node h = new Node(18, "H");
+
+
+
+        a.Left = b;
+        a.Right = c;
+        c.Right = g;
+        g.Right = h;
+        c.Left = f;
+        b.Right = e;
+        b.Left = d;
+
+       System.out.print(NodeFinder(a, e));
+
+
+       
+
+
+
+        /*
+                  a
+                /    \
+                b      c
+               /\      /\
+              d  e    f  g
+                          \ 
+                            h
+
+        */
+     
+
+
+
+
+          }
+
+ static boolean NodeFinder(Node root, Node node){
+                
+                 Node curr = root;
+                 
+                 
+                 
+                 while(curr != null){
+                      
+                    if(node.Name.equals(curr.Name))  return true;
+                    else if( node.data > curr.data)  curr = curr.Right;
+                    else {curr = curr.Left;}
+                 }
+               return false;
+
+
+      }
+
+    static void pratice(Node root){
+             Stack<Node> stack = new Stack<Node>();
+             Node curr = root;
+               
+                
+
+             while( curr != null || !stack.isEmpty()){
+                         while(curr != null){
+
+                                 stack.push(curr);
+                                 System.out.println(curr.Name);
+                                curr = curr.Left;
+                                  
+                                 
+                                  
+                                  
+                         }
+                         curr = stack.pop();
+                        
+                         curr = curr.Right;
+                         
+             }
+             
+
+    }
+static void RecursivePostOrderTreeTraveler(Node root){
+             Node curr = root;
+
+             if(curr.Left != null) RecursivePostOrderTreeTraveler(curr.Left);
+               
+               if(curr.Right != null) RecursivePostOrderTreeTraveler(curr.Right);
+                 System.out.println(curr.Name);
+
+
+
+
+}
+
+
+
+       static void PostOrderTreeTraveler(Node root){
+               
+                Stack<Node> stack = new  Stack<Node>();
+              Stack<Node> stack1 = new Stack<Node>();
+               
+               
+               
+               Node curr = root;
+
+               stack.push(curr);
+
+
+             
+
+              while(!stack.isEmpty()){
+                Node temp = stack.pop();
+                stack1.push(temp);
+
+                if ( temp.Left != null) stack.push(temp.Left);
+                if ( temp.Right != null) stack.push(temp.Right);
+                  
+
+              }
+
+              while(!stack1.isEmpty()){
+                Node Output = stack1.pop();
+                System.out.println(Output.Name + " visited");
+              }
+
+
+       }
+
+
+
+    static void PreOrderTreeTraveler(Node root){
+       Stack<Node> stack = new Stack<Node>();
+         Node curr = root;
+         curr = stack.push(curr);
+         
+            while(!stack.isEmpty()){
+              Node temp = stack.pop();
+              
+              System.out.println(temp.Name);
+              if(temp.Right != null) stack.push(temp.Right);
+    
+               if(temp.Left != null) stack.push(temp.Left);
+                 
+            }
+              
+         
+
+
+
+
+    }
+
+
+    static void InOrderTreeTraveler(Node root){
+           Stack<Node> stack = new Stack<Node>();
+             Node current = root;
+              
+           while(!stack.isEmpty() || current != null){
+
+                  while(current != null){
+                    stack.push(current);
+                    current = current.Left;
+                   
+                    
+                  }
+                 current =  stack.pop();
+                 System.out.println(current.Name + " =visited");
+
+                 current = current.Right;
+                
+
+           }
+                          
+                 
+
+
+
+            }
+
+      
+
+    
+    
+
+
+    static boolean RecursiveNodeFinder(Node root, String Name){
+         
+
+      if(root == null) return false;
+      if(root.Name.equals(Name)) return true;
+      
+
+      return RecursiveNodeFinder(root.Left, Name) || RecursiveNodeFinder(root.Right, Name);
+
+
+
+    }
+ static void BFS( Node root ){
+         Queue<Node> queue = new LinkedList<Node>();
+
+         queue.offer(root);
+         
+
+
+         while(!queue.isEmpty()){
+             Node current = queue.poll();
+             System.out.println(current.Name + " = visited");
+
+             if(current.Left != null) queue.offer(current.Left);
+             if(current.Right != null) queue.offer(current.Right);
+
+            
+         }
+
+
+ }
+
+
+
+
+
+}
---
+++ Node.java
@@ -0,0 +1,18 @@
+public class Node {
+int data;
+String Name;
+
+Node Right;
+Node Left;
+
+
+Node(int data, String Name){
+    this.data = data;
+    this.Name = Name;
+
+
+}
+
+
+
+}
